7af741ec5c5d0e3d49814248dddd6c73baac2664
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-06-10 20:11:34 +0200

    fix number of blank lines

--- figs/style.py
+++ figs/style.py
@@ -4,7 +4,6 @@ Global style configuration for all plot scripts.
 
 COLORSCALE: str = "viridis"
 
-
 PLOT_TITLE_FONTSIZE: float = 24
 XY_AXIS_CBAR_TITLE_FONTSIZE: float = 20
 TICK_LABEL_FONTSIZE: float = 18

aa18206eb551f675913bb6b3f9bf4456e830e6f2
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-09-03 22:20:13 +0200

    add bar plot

---
+++ figs/citation_over_year/citations_over_year.py
@@ -0,0 +1,53 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "kaleido",
+#     "pandas",
+#     "plotly",
+# ]
+# ///
+
+"""
+Plot citations of 1st pymatgen paper over year bar plot.
+
+https://scholar.google.com/scholar?hl=en&as_sdt=2005&sciodt=0,5&cites=6511812476881528112&scipsc=&q=#d=gs_md_hist&t=1756930044910
+"""
+
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv("citations.csv")
+
+fig = px.bar(
+    df,
+    x="Year",
+    y="Citations",
+    labels={"Year": "Year", "Citations": "Number of Citations"},
+)
+
+fig.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",  # remove background (inside plot)
+    paper_bgcolor="rgba(0,0,0,0)",  # remove outer background
+    title=dict(
+        text="1<sup>st</sup> pymatgen Paper Citations Per Year",
+        x=0.5,
+        font=dict(size=24),
+    ),
+    xaxis=dict(
+        title=dict(text="Year", font=dict(size=18)),
+        tickfont=dict(size=14),
+        showgrid=False,
+    ),
+    yaxis=dict(
+        title=dict(text="Number of Citations", font=dict(size=18)),
+        tickfont=dict(size=14),
+        showgrid=True,
+        gridcolor="lightgray",
+        gridwidth=2,
+        griddash="dot",
+    ),
+)
+
+# fig.show()
+fig.write_image("citations.svg")

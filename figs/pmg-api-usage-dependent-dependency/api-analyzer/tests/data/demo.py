5b9c0f844e36d7eaa6757240602bb9b1a6a8e5ea
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-10-06 16:44:20 +0200

    add test for `if typing.TYPE_CHECKING:`

--- figs/pmg-api-usage-dependent-dependency/api-analyzer/tests/data/demo.py
+++ figs/pmg-api-usage-dependent-dependency/api-analyzer/tests/data/demo.py
@@ -1,12 +1,19 @@
+import typing
 from typing import TYPE_CHECKING
 
 import mypkg.core as core
 
-if TYPE_CHECKING:
+if typing.TYPE_CHECKING:
     # Ensure type checking import is not recorded
     from numpy.typing import NDArray
 
     _: NDArray | None = None
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    _: NDArray | None = None
+
 
 core.run()
 
